8ab8933d5a2ef365f6b014c59e431460814456ae
David Peter <david.lonyai@gmail.com>
2023-06-04 11:44:50 +0200

    feat: pretty and json formatters

--- file_handler.ts
+++ file_handler.ts
@@ -1,8 +1,8 @@
 import { config } from "./config.ts";
 import { FileHandler } from "./deps.ts";
-import { formatter } from "./formatter.ts";
+import { json } from "./formatter.ts";
 
 export const file_handler = new FileHandler(config.level, {
   filename: config.filename,
-  formatter,
+  formatter: json,
 });
--- formatter.ts
+++ formatter.ts
@@ -1,12 +1,18 @@
 import { formatDate, LogRecord } from "./deps.ts";
 import stringify from "./stringify.ts";
 
-export const formatter = (record: LogRecord) =>
+export const pretty = (record: LogRecord) =>
   [
-    `[${record.levelName}] ${
-      formatDate("yyyy MM dd mm:ss", record.datetime)
-    } ${record.msg}`,
+    `[${record.levelName}] ${formatDate("yyyy MM dd mm:ss", record.datetime)} ${
+      record.msg
+    }`,
     ...record.args.map((arg) =>
       typeof arg === "object" ? stringify(arg, 4) : `${arg}`
     ),
   ].join(" ");
+
+export const json = (record: LogRecord) =>
+  stringify(
+    { ...record, datetime: record.datetime.toISOString(), args: record.args },
+    0
+  );
